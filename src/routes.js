da734e3cb119fb62b09f3caa22986c6a5e5ab3ce
valdir-ti <valdir.ti@gmail.com>
2020-01-22 17:56:58 -0300

    Adicionando Bootstrap ao projeto

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Menus from './pages/Menus';
