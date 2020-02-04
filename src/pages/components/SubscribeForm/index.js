55dff3d5938673ec751c5f96e941024f94b5141d
valdir-ti <valdir.ti@gmail.com>
2020-02-04 17:47:58 -0300

    Inserindo emails pela API na página inicial

--- src/pages/components/SubscribeForm/index.js
+++ src/pages/components/SubscribeForm/index.js
@@ -1,28 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../../services/api';
+import { Form } from './styles';
 
-export default function SubscribeForm() {
-  return (
-    <>
-      <form className="form-inline">
-        <div className="col-md-6 col-sm-6">
-          <div className="form-group">
-            <label htmlFor="email" className="sr-only">
-              E-mail
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="Seu E-mail"
-            />
+export default class SubscribeForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
+
+  handleEmailChange = event => {
+    this.setState({ email: event.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { email } = this.state;
+    api
+      .post('emails', {
+        email,
+      })
+      .then(res => {
+        console.log(res.status);
+        if (res.status === 201) {
+          setTimeout(
+            function() {
+              this.setState({ email: '' });
+            }.bind(this),
+            1000
+          );
+        }
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <Form className="form-inline" onSubmit={this.handleSubmit}>
+          <div className="col-md-6 col-sm-6">
+            <div className="form-group">
+              <label htmlFor="email" className="sr-only">
+                E-mail
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                autocomplete="off"
+                id="email"
+                placeholder="Seu E-mail"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleEmailChange}
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-md-6 col-sm-6">
-          <button type="submit" className="btn btn-default btn-block">
-            Inscrever
-          </button>
-        </div>
-      </form>
-    </>
-  );
+          <div className="col-md-6 col-sm-6">
+            <button type="submit" className="btn btn-default btn-block">
+              Inscrever
+            </button>
+          </div>
+        </Form>
+      </>
+    );
+  }
 }
---
+++ src/pages/components/SubscribeForm/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  button:hover {
+    background: #e4a13c !important;
+  }
+`;
