983f382bdef3d262290d633279c42141e214c123
valdir-ti <valdir.ti@gmail.com>
2020-02-13 17:19:21 -0300

    Inserção de um gráfico simples

--- src/pages/Admin2/Dashboard/index.js
+++ src/pages/Admin2/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Content, Row, Col, Box, Button } from 'adminlte-2-react';
+import { Chart } from 'react-google-charts';
 
 export default class Dashboard extends Component {
   state = {};
@@ -14,7 +15,43 @@ export default class Dashboard extends Component {
       >
         <Row>
           <Col xs={12}>
-            <Box
+            <Chart
+              chartType="ScatterChart"
+              rows={[
+                [8, 12],
+                [4, 5.5],
+                [11, 14],
+                [4, 5],
+                [3, 3.5],
+                [6.5, 7],
+              ]}
+              columns={[
+                {
+                  type: 'number',
+                  label: 'Idade',
+                },
+                {
+                  type: 'number',
+                  label: 'Peso',
+                },
+              ]}
+              options={
+                // Chart options
+                {
+                  title: 'Comparação Idade vs. Peso',
+                  hAxis: {
+                    title: 'Idade',
+                    viewWindow: { min: 0, max: 15 },
+                  },
+                  vAxis: { title: 'Peso', viewWindow: { min: 0, max: 15 } },
+                  legend: 'none',
+                }
+              }
+              width={'100%'}
+              height={'400px'}
+              legendToggle
+            />
+            {/* <Box
               title="My first box"
               type="primary"
               closable
@@ -22,7 +59,7 @@ export default class Dashboard extends Component {
               footer={<Button type="danger" text="Danger Button" />}
             >
               Projeto Saboroso
-            </Box>
+            </Box> */}
           </Col>
           <Col xs={12}>
             <Box title="Another box">Content goes here</Box>
